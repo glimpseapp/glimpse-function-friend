51841fe014bb33a960d42e6851b2db60a6a55874
Federico Ulfo <federicoulfo@gmail.com>
2017-09-29 16:38:25 +0200

    Add friends request and friends list

--- functions/index.js
+++ functions/index.js
@@ -10,8 +10,6 @@ const app = express();
 
 // Validate the user is logged in taking the Firebase JWT, and adding uid and email to the req.user
 const validateFirebaseIdToken = (req, res, next) => {
-    console.log('Check if request is authorized with Firebase ID token');
-
     if (req.originalUrl == '/healthz') {
         return res.send({status: true});
     }
@@ -24,7 +22,7 @@ const validateFirebaseIdToken = (req, res, next) => {
     let idToken = req.headers.authorization.split('Bearer ')[1];
 
     admin.auth().verifyIdToken(idToken).then(decodedIdToken => {
-        console.log('ID Token correctly decoded', decodedIdToken);
+        console.log('Authenticated ', decodedIdToken.email);
         req.user = decodedIdToken;
         next();
     }).catch(error => {
@@ -34,6 +32,10 @@ const validateFirebaseIdToken = (req, res, next) => {
 };
 
 app.use(validateFirebaseIdToken);
+
+app.get('/friends', (req, res) => {
+    admin.database().ref(`friends/${req.user.uid}`).once('value').then(requests => res.send(requests.val()));
+});
 
 app.get('/requests', (req, res) => {
     admin.database().ref(`friend-requests/received/${req.user.uid}`).once('value').then(requests => res.send(requests.val()));
@@ -47,12 +49,18 @@ app.post('/request', (req, res) => {
         receiver_id: receiverId,
         ts: Date.now()
     };
-    admin.database().ref(`friend-requests/sent/${senderId}/${receiverId}`).set(data, function (error) {
-        admin.database().ref(`friend-requests/received/${receiverId}/${senderId}`).set(data, function (error) {
-            if (error)
-                res.send({error: error})
+    admin.database().ref(`friend-requests/sent/${senderId}/${receiverId}`).set(data, function (error1) {
+        admin.database().ref(`friend-requests/received/${receiverId}/${senderId}`).set(data, function (error2) {
+            var errors = []
+            if (error1)
+                errors.push(error1)
+            if (error2)
+                errors.push(error2)
+            if (error1 || error2)
+                res.send({error: errors})
             else
-                res.send({success: true});
+
+            res.send({success: true});
         });
     });
 });
@@ -60,10 +68,44 @@ app.post('/request', (req, res) => {
 app.post('/accept', (req, res) => {
     const senderId = req.body.sender_id;
     const receiverId = req.user.uid;
+    admin.database().ref(`friend-requests/received/${receiverId}/${senderId}`).once('value').then(data => {
+        admin.database().ref(`friend-requests/received/${receiverId}/${senderId}`).remove()
+        admin.database().ref(`friend-requests/sent/${senderId}/${receiverId}`).remove()
+
+        const friend_data1 = {
+            user_id: receiverId,
+            rel_user_id: senderId
+        }
+        const friend_data2 = {
+            user_id: senderId,
+            rel_user_id: receiverId
+        }
+
+        admin.database().ref(`friends/${receiverId}/${senderId}`).set(friend_data1, function (error1) {
+            admin.database().ref(`friends/${senderId}/${receiverId}`).set(friend_data2, function (error2) {
+                var errors = []
+                if (error1)
+                    errors.push(error1)
+                if (error2)
+                    errors.push(error2)
+                if (error1 || error2)
+                    res.send({error: errors})
+                else
+                    res.send({success: true});
+            });
+        });
+
+
+    });
+});
+
+app.post('/reject', (req, res) => {
+    const senderId = req.body.sender_id;
+    const receiverId = req.user.uid;
     admin.database().ref(`friend-requests/received/${receiverId}/${senderId}`).once('value').then(friend_request => {
         admin.database().ref(`friend-requests/received/${receiverId}/${senderId}`).remove()
         admin.database().ref(`friend-requests/sent/${senderId}/${receiverId}`).remove()
-        res.send({success:true})
+        res.send({success: true})
     });
 });
 
